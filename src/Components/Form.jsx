51a4f20a87cf74589b5a8400ba2e18ac07c31b79
Roman Viitovych <114577315+romanvtch@users.noreply.github.com>
2023-04-15 16:57:41 +0200

    beta v0.1

---
+++ src/Components/Form.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+export const Form = () => {
+  const [isName, setIsName] = useState("");
+  const [isEmail, setIsEmail] = useState("");
+  const [isMessage, setIsMessage] = useState("");
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSumbit}>
+      <label htmlFor="name">Name *</label>
+      <br/>
+      <input
+        type="text"
+        id="name"
+        value={isName}
+        onChange={(e) => setIsName(e.target.value)}
+      />
+
+      <br />
+
+      <label htmlFor="email">Email *</label>
+      <br/>
+      <input
+        type="email"
+        id="email"
+        value={isEmail}
+        onChange={(e) => setIsEmail(e.target.value)}
+      />
+      <br />
+
+      <label htmlFor="message">Message *</label>
+      <br />
+      <textarea
+        id="message"
+        value={isMessage}
+        onChange={(e) => setIsMessage(e.target.value)}
+      />
+      <br />
+
+      <button type="submit">Send Message</button>
+    </form>
+  );
+};
